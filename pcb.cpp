a0788c4fd9575ef56bb51f4e4891c6e5a580da5e
dyer004 <dyer004@cougars.csusm.edu>
2014-09-01 13:12:48 -0700

    Update pcb.cpp

--- pcb.cpp
+++ pcb.cpp
@@ -5,3 +5,45 @@
  * Bryce Kinney and Daniel Dyer
  * Version 0.1 08/29/14
  */
+ 
+#include "PCB.h"
+#include <iostream>
+using namespace std;
+ 
+PCB::PCB()
+{
+    int ID = 0;
+    int priority = 0;
+    string state = "";
+}
+
+int PCB::getID()
+{
+    return ID;
+}
+
+int PCB::getPriority()
+{
+    return priority;
+}
+
+string PCB::getState()
+{
+    return state;
+}
+
+void PCB::setID(int x)
+{
+    ID = x;
+}
+
+void PCB::setPriority(int x)
+{
+    priority = x;
+}
+
+void PCB::setState(string x)
+{
+    state = x;
+}
+ 
